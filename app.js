fd69baa44f6718c7e7653d107cbb4fdd38aa05e6
Jihee Byun <jh910805jh@gmail.com>
2024-07-11 22:32:10 +0900

    [feat] added D3 V6

---
+++ app.js
@@ -0,0 +1,9 @@
+
+console.log("imported")
+const body = d3.select('body')
+                .append('p') // add elements 
+                .attr('font-size','20px')
+                .text('HELLO')
+
+console.log(body)
+
